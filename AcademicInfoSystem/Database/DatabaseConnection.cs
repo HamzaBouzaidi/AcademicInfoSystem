aecbef397987643b0d466bec8efd584a59eaef02
HamzaBouzaidi <Hamzabzd91@gmail.com>
2025-11-25 19:15:08 +0200

    Add project files.

---
+++ AcademicInfoSystem/Database/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicInfoSystem.Database
+{
+    internal class DatabaseConnection
+    {
+        private static string connectionString = "Server=localhost;Database=AcademicSystem;User=root;Password=;";
+
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
